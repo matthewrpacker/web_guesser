44f7415bba73d3446386bda21951c88e04d862ec
matthewrpacker <matthewrpacker@gmail.com>
2016-06-20 10:29:08 -0600

    Add color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,24 +1,33 @@
 require 'sinatra'
-require 'sinatra/reloader'
-
-SECRET_NUMBER = rand(100)
 
 get '/' do
-  guess = params["guess"].to_i
+  guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, {locals: {message: message, color: @color}}
 end
 
 def check_guess(guess)
-  if (guess - 5) > SECRET_NUMBER
+  if guess.nil?
+    $secret_number = rand(100)
+    @color = "skyblue"
+    return "Take a guess!"
+  end
+
+  guess = guess.to_i
+  if (guess - 5) > $secret_number
+    @color = "red"
     "Way too high!"
-  elsif (guess + 5) < SECRET_NUMBER
+  elsif (guess + 5) < $secret_number
+    @color = "red"
     "Way too low!"
-  elsif guess < SECRET_NUMBER
+  elsif guess < $secret_number
+    @color = "rgb(255, 111, 0)"
     "Too low!"
-  elsif guess == SECRET_NUMBER
-    "You got it right! The secret_number is #{SECRET_NUMBER}"
-  elsif guess > SECRET_NUMBER
+  elsif guess == $secret_number
+    @color = "green"
+    "You got it right! The secret number is #{$secret_number}"
+  elsif guess > $secret_number
+    @color = "rgb(255, 111, 0)"
     "Too high!"
   end
 end
